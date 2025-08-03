3b533406e598fc0cd161ac7f7d605e168e7aa0d5
HossainMaruf <maruficepustian@gmail.com>
2025-08-03 12:08:38 +0600

    Initial

---
+++ Basic Input Output/2. Commenting.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+    printf("Hello World");
+
+    // printf("Bangladesh");
+    // printf("C Programming");
+    // This is single line comment
+
+    /* This is
+        multiline comment
+        of c programming language
+    */
+
+    /**
+     * This is perfect multiline comment
+     * This is the feature of VS code
+     * This is not the part of c programming feature
+     */
+
+}
---
+++ Basic Input Output/3. BackslashConstant.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    printf("Hello\nWorld\n");
+    printf("Bangla\tdesh\n");
+    printf("\nC Programming");
+}
+
+/** OUTPUT WILL BE:
+
+    Hello
+    World
+    Bangla  desh
+
+    C Programming
+
+ */
