559a82960d058a882024fc0cfd0586600ceea9d0
HossainMaruf <maruficepustian@gmail.com>
2025-08-03 23:21:27 +0600

    BasicArithmatic added

---
+++ Basic Input Output/5. BasicArithmatic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main() {
+    printf("Sum = %d\n", 10+3); // 13
+    printf("Minus = %d\n", 10-3); // 7
+    printf("Multiplication = %d\n", 10*3); // 30
+    printf("Quotient = %d\n", 10/3); // 3
+    printf("Remainder = %d\n", 10%3); // 1 (Mod = Modulus)
+
+
+    printf("Sum = %f\n", 10.5+3.5);
+    printf("Minus = %f\n", 10.5-3.5);
+    printf("Multiplication = %f\n", 10.5*3.5);
+    printf("Division = %f\n", 10.5/3.5);
+    // printf("Remainder = %d\n", 10.5%3.5); // ERROR
+}
