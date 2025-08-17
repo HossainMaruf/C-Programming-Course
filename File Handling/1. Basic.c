0c242ce25011a73d979d1489a82687d29229da95
HossainMaruf <maruficepustian@gmail.com>
2025-08-17 23:56:52 +0600

    rewind and ftell function

--- File Handling/1. Basic.c
+++ File Handling/1. Basic.c
@@ -4,10 +4,28 @@ void main() {
     file = fopen("record.txt", "r");
     if(file == NULL) printf("File is not opened\n");
     else {
-        char ch;
-        while((ch = fgetc(file)) != -1) {
-            putchar(ch);
-        }
+        // char ch;
+        // while((ch = fgetc(file)) != -1) {
+        //     putchar(ch);
+        // }
+        // putchar(fgetc(file));
+        // putchar(fgetc(file));
+        // putchar(fgetc(file));
+        // rewind(file); // resetting the file pointer
+        // putchar(fgetc(file));
+        // putchar(fgetc(file));
+        long value = ftell(file);
+        printf("%ld\n", value);
+
+        fgetc(file);
+
+        value = ftell(file);
+        printf("%ld\n", value);
+
+        fgetc(file);
+        value = ftell(file);
+        printf("%ld\n", value);
+
     }
     fclose(file);
 }
