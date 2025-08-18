f35d410ef7dcf43067d4a7f4edd95b10401d2554
HossainMaruf <maruficepustian@gmail.com>
2025-08-19 00:27:29 +0600

    print random line

--- File Handling/3. fgets.c
+++ File Handling/3. fgets.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#define WIN_EXTRA_CHARS 2
+int lineLength(FILE *file) {
+    int count=0;
+    while(fgetc(file) != '\n') count++;
+    return count;
+}
+void printLine(FILE *file, int lineNumber) {
+    int n = lineLength(file);
+    char buffer[n+WIN_EXTRA_CHARS];
+    fseek(file, (lineNumber-1)*(n+WIN_EXTRA_CHARS),SEEK_SET);
+    printf(fgets(buffer, sizeof(buffer), file));
+}
+
 void main() {
     FILE *file = NULL;
     file = fopen("record.txt", "r");
     if(file == NULL) printf("File is not opened\n");
     else {
-        char line[15];
-        printf(fgets(line, sizeof(line), file));
-        printf(fgets(line, sizeof(line), file));
-        printf(fgets(line, sizeof(line), file));
+        printLine(file, 2);
+        printLine(file, 3);
     }
     fclose(file);
 }
