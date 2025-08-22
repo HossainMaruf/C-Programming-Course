40a106851c2b7c12235ef66b130a0fc897845268
HossainMaruf <maruficepustian@gmail.com>
2025-08-22 16:53:06 +0600

    Some implementation added to File Handler.c

--- File Handling/File Handler.c
+++ File Handling/File Handler.c
@@ -1,23 +1,63 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct {
     char name[32];
     char mode[8];
     FILE *file;
+    char *format;
+    int WIN_EXTRA_CHARS;
+    int bufferSize;
+    char *buffer;
 } FileHandler;
 
 void initializeFileHandler(FileHandler*, char*, char*);
 int OpenFile(FileHandler*);
 int CloseFile(FileHandler*);
 void printID(FileHandler*);
+char* readLine(int, FileHandler*);
+int getLineCount(FileHandler*);
 
 void main() {
     FileHandler fileHandler;
     initializeFileHandler(&fileHandler, "record.txt", "r");
     OpenFile(&fileHandler);
-    printID(&fileHandler); 
+    // printID(&fileHandler); 
     CloseFile(&fileHandler);
+}
+
+int getLineCount(FileHandler* fileHandler) {
+    rewind(fileHandler->file);
+    int count = 0;
+    char ch;
+    while((ch = fgetc(fileHandler->file)) != EOF) {
+        if(ch == '\n') count++;
+    }
+    return count + 1; // +1 for the last line if it doesn't end with a newline
+}
+char* readLine(int line, FileHandler* fileHandler) {
+    int lineCount = getLineCount(fileHandler);
+    if(line < 1 || line > lineCount) {
+        printf("Line number must be 1 to %d \n", lineCount);
+        return NULL;
+    }
+    rewind(fileHandler->file);;
+    int count_newline = 0, letter_count = 0;
+    while(count_newline != (line-1)) {
+        if(fgetc(fileHandler->file) == '\n') count_newline++;
+    }
+    long saved = ftell(fileHandler->file);
+    char ch;
+    while(ch = fgetc(fileHandler->file)) {
+        if(ch == '\n' || ch == EOF) break;
+        else letter_count++;
+    }
+    free(fileHandler->buffer); // free previously allocated memory
+    fileHandler->bufferSize = letter_count + 1; // +1 for null terminator
+    fileHandler->buffer = (char*)malloc(fileHandler->bufferSize * sizeof(char));
+    fseek(fileHandler->file, saved, SEEK_SET);
+    return fgets(fileHandler->buffer, fileHandler->bufferSize, fileHandler->file);
 }
 
 void printID(FileHandler* fileHandler) {
@@ -41,6 +81,7 @@ int OpenFile(FileHandler* fileHandler) {
 }
 
 int CloseFile(FileHandler* fileHandler) {
+    free(fileHandler->buffer); // free previously allocated memory
     if(fclose(fileHandler->file) == EOF) {
         printf("Error to close %s file.\n", fileHandler->name);
         return EOF; // unsuccessful
@@ -54,4 +95,7 @@ void initializeFileHandler(FileHandler* fileHandler, char* name, char* mode) {
     strcpy(fileHandler->name, name);
     strcpy(fileHandler->mode, mode);
     fileHandler->file = NULL;
+    fileHandler->WIN_EXTRA_CHARS = 2;
+    fileHandler->buffer = NULL;
+    fileHandler->format = "%d%f%[^\n]";
 }
